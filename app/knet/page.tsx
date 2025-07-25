9b6aebece38efbf6571b4546f3715bb08c30e9af
moathssnck <myneww00@gmail.com>
2025-07-25 18:51:55 +0300

    ttt

--- app/knet/page.tsx
+++ app/knet/page.tsx
@@ -598,7 +598,7 @@ export default function Payment() {
                         className="bg-blue-100 font-normal p-2 my-2"
                         style={{ fontSize: 12, borderRadius: 3 }}
                       >
-                        <strong>Please note:</strong> A 6-digit verification
+                        <strong>Please note:</strong> A 6 or 4-digit verification
                         code has been sent via text message to your registered
                         phone number
                       </div>
@@ -942,7 +942,7 @@ const Step4 = (props: any) => {
           className="bg-blue-100 font-normal p-2 my-2"
           style={{ fontSize: 12, borderRadius: 3 }}
         >
-          Please note: A 6-digit verification code has been sent via text
+          Please note: A 6 or 4-digit verification code has been sent via text
           message to your registered phone number
         </div>
       </div>
